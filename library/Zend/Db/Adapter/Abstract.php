3d89286428d6c080111b2a6cc8d8927f57d451fa
Octav <tavy315@users.noreply.github.com>
2018-07-26 12:02:39 +0300

    Update Abstract.php

--- library/Zend/Db/Adapter/Abstract.php
+++ library/Zend/Db/Adapter/Abstract.php
@@ -919,10 +919,10 @@ abstract class Zend_Db_Adapter_Abstract
      * // $safe = "WHERE date < '2005-01-02'"
      * </code>
      *
-     * @param string  $text  The text with a placeholder.
-     * @param mixed   $value The value to quote.
-     * @param string  $type  OPTIONAL SQL datatype
-     * @param integer $count OPTIONAL count of placeholders to replace
+     * @param string      $text  The text with a placeholder.
+     * @param mixed       $value The value to quote.
+     * @param null|string $type  OPTIONAL SQL datatype
+     * @param integer     $count OPTIONAL count of placeholders to replace
      * @return string An SQL-safe quoted value placed into the original text.
      */
     public function quoteInto($text, $value, $type = null, $count = null)
